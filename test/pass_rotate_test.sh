3c0e497916eda75c314cb07b1a85ceaf4bd3194c
ben <ben@ros.io>
2019-08-02 16:51:25 -0600

    Move from get-password to get-account

--- test/pass_rotate_test.sh
+++ test/pass_rotate_test.sh
@@ -68,3 +68,30 @@ prepare pg: pass
 execute pg: pass new-pass
 EOF
 }
+
+@test "Able to use get-account" {
+    echo pass > "$TMPDIR/test-noop"
+
+    cat <<EOF > "$TMPDIR/pass-rotate.ini"
+[pass-rotate]
+get-account=echo '{ "password": "'\$(cat "$TMPDIR/test-noop")'", "username": "pg" }'
+set-password=tee "$TMPDIR/test-\$ACCOUNT"
+gen-password=echo "new-pass"
+
+[noop]
+cassette=$TMPDIR/cassette
+EOF
+
+    run $BATS_TEST_DIRNAME/../pass-rotate noop
+    assert_success
+    assert_line 'Rotating noop... OK'
+
+    run cat "$TMPDIR/test-noop"
+    assert_output new-pass
+
+    run cat "$TMPDIR/cassette"
+    cat <<'EOF' | assert_output
+prepare pg: pass
+execute pg: pass new-pass
+EOF
+}
